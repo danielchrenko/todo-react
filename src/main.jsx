b6e7036d695535ffd08cba7cacaf12ed219b71e4
danielchrenko <danchrenko1@gmail.com>
2023-12-29 19:39:45 -0800

    component-ified the list (made todo.jsx), and simplified main using iteration

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const DATA = [
+
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false },
+
+];
+// using all caps for the constant to indicate that the
+// data wont change but in the real application this changes
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App subject="Claurice"/>
+    <App tasks={DATA}/>
   </React.StrictMode>,
 )
